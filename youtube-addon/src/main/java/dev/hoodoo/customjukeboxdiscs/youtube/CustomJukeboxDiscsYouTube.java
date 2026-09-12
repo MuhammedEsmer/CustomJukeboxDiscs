fc1d11ffba18cc113de4aba9a4ec1509d4d3550e
Muhammed Esmer <125984857+MuhammedEsmer@users.noreply.github.com>
2026-09-12 20:01:24 +0300

    Scaffold YouTube server addon

---
+++ youtube-addon/src/main/java/dev/hoodoo/customjukeboxdiscs/youtube/CustomJukeboxDiscsYouTube.java
@@ -0,0 +1,11 @@
+package dev.hoodoo.customjukeboxdiscs.youtube;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(CustomJukeboxDiscsYouTube.MOD_ID)
+public final class CustomJukeboxDiscsYouTube {
+    public static final String MOD_ID = "customjukeboxdiscsyoutube";
+
+    public CustomJukeboxDiscsYouTube() {
+    }
+}
